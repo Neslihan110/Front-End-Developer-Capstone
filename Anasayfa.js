067f746a45cd50bab36dd43126ebd596834a6182
Neslihan <116304785+Neslihan110@users.noreply.github.com>
2023-04-21 12:27:27 +0300

    Create Anasayfa.js

---
+++ Anasayfa.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/header";
+import Footer from "./Components/footer";
+import Hero from "./Components/hero";
+
+
+function HomePage(){
+    return (
+        <>
+        <Header></Header>
+        <Hero></Hero>
+        <Footer></Footer>
+        </>
+    );
+}
+
+export default HomePage
